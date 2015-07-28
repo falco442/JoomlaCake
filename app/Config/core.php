9fcb037c214f945a1432dbf4841575f553579870
falco442 <marcofra83@gmail.com>
2015-07-28 13:22:11 +0200

    modified core.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -227,7 +227,7 @@
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '123');
+	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
